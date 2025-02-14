be1442c0c0a951dd5a59a126ec3c0098227a442b
Alex-Guima <alexandreagtaveira@gmail.com>
2025-02-14 09:58:26 -0300

    Create the Library class as a Singleton
    Created the Library.java file and wrote some logic to fit this class
    into the Singleton design pattern

--- simples/projeto/src/main/java/Library.java
+++ simples/projeto/src/main/java/Library.java
@@ -1,5 +1,23 @@
-public class Library {
-    public static void main(String[] args) {
+public final class Library {
 
+    private static volatile Library instance;
+
+    public String value;
+
+    private Library(String value) {
+        this.value = value;
+    }
+
+    public static Library getInstance(String value) {
+        Library result = instance;
+        if (result != null) {
+            return result;
+        }
+        synchronized (Library.class) {
+            if (instance == null) {
+                instance = new Library(value);
+            }
+            return instance;
+        }
     }
 }
