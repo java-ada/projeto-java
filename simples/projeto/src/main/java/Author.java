2927e12dc382562148ee0243159b4291b446e1c7
jgribeiro83 <joaogabrielrr@yahoo.com.br>
2025-02-14 20:26:33 -0300

    Alterações feitas em Autor e eBook

--- simples/projeto/src/main/java/Author.java
+++ simples/projeto/src/main/java/Author.java
@@ -1,11 +1,11 @@
+import java.time.LocalDate;
+import java.util.List;
+
 public class Author {
     private String name;
     private String email;
-
-    public Author(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
+    private LocalDate dob;  //data de nascimento
+    private List<Book> books;
 
     public String getName() {
         return name;
@@ -19,13 +19,30 @@ public class Author {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setEmail(String email) { this.email = email; }
+
+    public LocalDate getDob() {return dob; }
+
+    public void setDob(LocalDate dob) {
+        if (dob.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException("Data inválida: Data inserida está no futuro");
+        } else {
+            this.dob = dob;
+        }
     }
 
-    @Override
-    public String toString() {
-        return "{name='" + name + "'}";
-        //return "Autor{name='" + name + "', email='" + email + "'}";
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void printAllBooks() {
+        System.out.println(this.getName() + " é autor dos seguintes livros: ");
+        for (Book book : books) {
+            System.out.println(book.getTitle() + ", " + book.getPages() + " Páginas");
+        }
+    }
+
+    public List<Book> getBooks() {
+        return books;
     }
 }
