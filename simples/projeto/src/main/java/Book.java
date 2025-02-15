eff1032b1ae955bb71f9472179ba177b459d051a
Alex-Guima <alexandreagtaveira@gmail.com>
2025-02-15 09:40:03 -0300

    Start Library class implementation and Create Validator class
    In order to reduce the complexity of the Main class, refactored
    the book managing methods into the Library class, so that the Main
    calls the Methods from Library as needed
    
    Created Validator class in order to check if the ISBN13 input is
    actually valid, based on the checking digit of the ISBN13 String

--- simples/projeto/src/main/java/Book.java
+++ simples/projeto/src/main/java/Book.java
@@ -1,8 +1,5 @@
 import java.util.Arrays;
-
 import java.util.stream.Collectors;
-
- 
 
 public abstract sealed class Book permits PhysicalBook, eBook {
 
@@ -13,10 +10,16 @@ public abstract sealed class Book permits PhysicalBook, eBook {
     private final String publisher;
     private final Author author;
     private final Genre[] genres;
- 
 
-    public Book(String ISBN13, String title, String description, int pages, String publisher, Author author, Genre[] genres) {
-
+    public Book(
+        String ISBN13,
+        String title,
+        String description,
+        int pages,
+        String publisher,
+        Author author,
+        Genre[] genres
+    ) {
         this.ISBN13 = ISBN13;
         this.title = title;
         this.description = description;
@@ -24,81 +27,66 @@ public abstract sealed class Book permits PhysicalBook, eBook {
         this.publisher = publisher;
         this.author = author;
         this.genres = genres;
-
     }
-
 
     public String getISBN13() {
-
         return ISBN13;
-
     }
- 
 
     public String getTitle() {
-
         return title;
-
     }
- 
 
     public String getDescription() {
-
         return description;
-
     }
-
 
     public int getPages() {
-
         return pages;
-
     }
- 
 
     public String getPublisher() {
-
         return publisher;
-
     }
-
 
     public Author getAuthor() {
-
         return author;
-
     }
- 
 
     public Genre[] getGenres() {
-
         return genres;
-
     }
- 
 
     public void details() {
-
         String bookAuthor = this.author.getName();
 
-        var categorias = Arrays.stream(this.genres).map(Genre::name).collect(Collectors.toSet());
+        var categorias = Arrays.stream(this.genres)
+            .map(Genre::name)
+            .collect(Collectors.toSet());
 
         var json =
+            """
 
-                """
+                ISBN13: %s,
+                Título: %s,
+                Descrição: %s,
+                Páginas: %d,
+                Editora: %s,
+                Autor: %s,
+                Categorias: %s,
 
-                    ISBN13: %s,
-                    Título: %s,
-                    Descrição: %s,
-                    Páginas: %d,
-                    Editora: %s,
-                    Autor: %s,
-                    Categorias: %s,
+            """;
 
-                """;
-
-        System.out.println(json.formatted(this.ISBN13, this.title, this.description, this.pages, this.publisher, bookAuthor, categorias));
-
+        System.out.println(
+            json.formatted(
+                this.ISBN13,
+                this.title,
+                this.description,
+                this.pages,
+                this.publisher,
+                bookAuthor,
+                categorias
+            )
+        );
     }
-
 }
--- simples/projeto/src/main/java/Main.java
+++ simples/projeto/src/main/java/Main.java
@@ -6,27 +6,32 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args)
+        throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
+        var library = new Library();
 
         String categorias =
-                """
-                    1- Romance       2- Suspense
-                    3- Fantasia      4- Ficção Científica
-                """;
+            """
+                1- Romance       2- Suspense
+                3- Fantasia      4- Ficção Científica
+            """;
 
-        Genre[] genre1 = {new Genre("Fantasia", "lol"), new Genre("Drama", "omg")};
+        Genre[] genre1 = {
+            new Genre("Fantasia", "lol"),
+            new Genre("Drama", "omg"),
+        };
         Author author1 = new Author("J.K. Rowling", "jkrowling@example.com");
 
         PhysicalBook book1 = new PhysicalBook(
-                "978-0747532699",
-                "Harry Potter and the Philosopher's Stone",
-                "A young wizard's journey begins.",
-                223,
-                "Bloomsbury",
-                author1,
-                genre1,
-                true
+            "978-0747532699",
+            "Harry Potter and the Philosopher's Stone",
+            "A young wizard's journey begins.",
+            223,
+            "Bloomsbury",
+            author1,
+            genre1,
+            true
         );
 
         List<PhysicalBook> books = getPhysicalBookList();
@@ -34,16 +39,17 @@ public class Main {
         boolean loggedIn = true;
 
         while (loggedIn) {
-
             Thread.sleep(5000);
 
             String menu =
-                    """
-  
-                            1- Adicionar um livro físico    2- Adicionar um livro digital   3- Pegar um livro emprestado 
-                            4- Deletar um livro             5- Listar todos os livros       6- Sair
-                    """;
-            System.out.println("Olá, bem-vindo à Caixoteca. Por favor, qual o seu nome?");
+                """
+
+                        1- Adicionar um livro físico    2- Adicionar um livro digital   3- Pegar um livro emprestado
+                        4- Deletar um livro             5- Listar todos os livros       6- Sair
+                """;
+            System.out.println(
+                "Olá, bem-vindo à Caixoteca. Por favor, qual o seu nome?"
+            );
             String user = scanner.nextLine();
             System.out.printf("Olá, %s. O que deseja?\n", user);
             System.out.println(menu);
@@ -53,57 +59,27 @@ public class Main {
 
             switch (option) {
                 case "1":
-                    System.out.println("Título: ");
-                    String title = scanner.nextLine();
-                    System.out.println("ISBN: ");
-                    String ISBN = scanner.nextLine();
-                    System.out.println("Descrição: ");
-                    String description = scanner.nextLine();
-                    System.out.println("Páginas: ");
-                    int pages = scanner.nextInt();
-                    scanner.nextLine();
-                    System.out.println("Editora: ");
-                    String publisher = scanner.nextLine();
-                    System.out.println("Nome do Autor: ");
-                    String authorName = scanner.nextLine();
-                    System.out.println("Email do Autor: ");
-                    String authorEmail = scanner.nextLine();
-                    Author author = new Author(authorName, authorEmail);
-                    System.out.println("Em quantas das cantegorias abaixo ele se encaixa?");
-                    System.out.println(categorias);
-                    int numberOfGenres = scanner.nextInt();
-                    scanner.nextLine();
-                    Genre[] genres = new Genre[numberOfGenres];
-                    for (int i = 0; i < numberOfGenres; i++) {
-                        System.out.println("Qual a categoria ele se encaixa? (Digite apenas 1 por vez): ");
-                        int categoria = scanner.nextInt();
-                        switch (categoria) {
-                            case 1:
-                                genres[i] = new Genre("Romance", "lol");
-                            case 2:
-                                genres[i] = new Genre("Suspense", "lmao");
-                            case 3:
-                                genres[i] = new Genre("Fantasia", "yippie");
-                            case 4:
-                                genres[i] = new Genre("Ficção Científica", "wow");
-                        }
-                        System.out.println(genres[i].name());
-                    }
-                    books.add(new PhysicalBook(ISBN, title, description, pages, publisher, author, genres, true));
-                    System.out.println("Livro adicionado! Voltando ao menu principal...");
+                    library.addBook();
+                    System.out.println(
+                        "Livro adicionado! Voltando ao menu principal..."
+                    );
                     break;
                 case "5":
                     System.out.println("Encontramos os seguintes livros: ");
-                    books.stream().map(Book::getTitle).forEach(System.out::println);
+                    books
+                        .stream()
+                        .map(Book::getTitle)
+                        .forEach(System.out::println);
                     break;
                 case "6":
-                    System.out.println("Obrigado e boa leitura. Até a próxima!");
+                    System.out.println(
+                        "Obrigado e boa leitura. Até a próxima!"
+                    );
                     loggedIn = false;
                     break;
-                case"7":
+                case "7":
                     book1.details();
                     break;
-
             }
         }
 
@@ -117,41 +93,44 @@ public class Main {
         Author author2 = new Author("George R.R. Martin", "grrm@example.com");
         Author author3 = new Author("J.R.R. Tolkien", "jrrtolkien@example.com");
 
-        Genre[] genre1 = {new Genre("Fantasy", "lmao"), new Genre("Drama", "lol")};
-        Genre[] genre2 = {new Genre("Adventure", "poggers")};
-        Genre[] genre3 = {new Genre("Epic", "sadge")};
+        Genre[] genre1 = {
+            new Genre("Fantasy", "lmao"),
+            new Genre("Drama", "lol"),
+        };
+        Genre[] genre2 = { new Genre("Adventure", "poggers") };
+        Genre[] genre3 = { new Genre("Epic", "sadge") };
 
         PhysicalBook book1 = new PhysicalBook(
-                "978-0747532699",
-                "Harry Potter and the Philosopher's Stone",
-                "A young wizard's journey begins.",
-                223,
-                "Bloomsbury",
-                author1,
-                genre1,
-                true
+            "978-0747532699",
+            "Harry Potter and the Philosopher's Stone",
+            "A young wizard's journey begins.",
+            223,
+            "Bloomsbury",
+            author1,
+            genre1,
+            true
         );
 
         PhysicalBook book2 = new PhysicalBook(
-                "978-0553103540",
-                "A Game of Thrones",
-                "The struggle for the Iron Throne.",
-                694,
-                "Bantam Books",
-                author2,
-                genre2,
-                true
+            "978-0553103540",
+            "A Game of Thrones",
+            "The struggle for the Iron Throne.",
+            694,
+            "Bantam Books",
+            author2,
+            genre2,
+            true
         );
 
         PhysicalBook book3 = new PhysicalBook(
-                "978-0618640157",
-                "The Lord of the Rings",
-                "An epic quest to destroy the One Ring.",
-                1178,
-                "George Allen & Unwin",
-                author3,
-                genre3,
-                true
+            "978-0618640157",
+            "The Lord of the Rings",
+            "An epic quest to destroy the One Ring.",
+            1178,
+            "George Allen & Unwin",
+            author3,
+            genre3,
+            true
         );
 
         books.add(book1);
@@ -159,5 +138,4 @@ public class Main {
         books.add(book3);
         return books;
     }
-
 }
