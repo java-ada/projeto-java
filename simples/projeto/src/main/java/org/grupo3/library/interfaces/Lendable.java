c464306b3ea5f4585266de8d3462426fad5e499b
Gustavo Aragão <gustavo.s.aragao.2003@gmail.com>
2025-02-17 21:18:27 -0300

    update

--- simples/projeto/src/main/java/org/grupo3/library/interfaces/Lendable.java
+++ simples/projeto/src/main/java/org/grupo3/library/interfaces/Lendable.java
@@ -1,8 +1,6 @@
 package org.grupo3.library.interfaces;
 
-import org.grupo3.library.entities.PhysicalBook;
-
 public interface Lendable {
-    void lendBook(PhysicalBook book);
-    void returnBook(PhysicalBook book);
+    void lendBook();
+    void returnBook();
 }
